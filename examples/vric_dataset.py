2f090f67c1e08f207188bf120075f2d951dcb184
Nikita Kasyanov <kasyanikita@gmail.com>
2024-02-28 10:08:31 +0300

    apply isort and yapf

--- examples/vric_dataset.py
+++ examples/vric_dataset.py
@@ -1,9 +1,5 @@
-from __future__ import absolute_import
-from __future__ import print_function
-from __future__ import division
+from __future__ import absolute_import, division, print_function
 
-import sys
-import os
 import os.path as osp
 
 import torchreid
